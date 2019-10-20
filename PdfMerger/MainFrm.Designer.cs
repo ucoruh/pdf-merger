b010b1ba2795babc62083a282de979ace54f3634
u_coruh@hotmail.com <u_coruh@hotmail.com>
2019-10-20 17:43:48 +0300

    initial commit

---
+++ PdfMerger/MainFrm.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace PDF571
+{
+    partial class MainFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnMoveDown = new System.Windows.Forms.Button();
+            this.btnMoveUp = new System.Windows.Forms.Button();
+            this.btnMergeAll = new System.Windows.Forms.Button();
+            this.listFiles = new System.Windows.Forms.ListBox();
+            this.btnMergeADF = new System.Windows.Forms.Button();
+            this.btnUpdateThesis = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRemove.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(147)))), ((int)(((byte)(181)))), ((int)(((byte)(179)))));
+            this.btnRemove.Enabled = false;
+            this.btnRemove.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRemove.Location = new System.Drawing.Point(528, 146);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(88, 34);
+            this.btnRemove.TabIndex = 12;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = false;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAdd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(147)))), ((int)(((byte)(181)))), ((int)(((byte)(179)))));
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(528, 100);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(88, 34);
+            this.btnAdd.TabIndex = 11;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnMoveDown
+            // 
+            this.btnMoveDown.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnMoveDown.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(147)))), ((int)(((byte)(181)))), ((int)(((byte)(179)))));
+            this.btnMoveDown.Enabled = false;
+            this.btnMoveDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMoveDown.Location = new System.Drawing.Point(528, 58);
+            this.btnMoveDown.Name = "btnMoveDown";
+            this.btnMoveDown.Size = new System.Drawing.Size(88, 34);
+            this.btnMoveDown.TabIndex = 10;
+            this.btnMoveDown.Text = "Move Down";
+            this.btnMoveDown.UseVisualStyleBackColor = false;
+            this.btnMoveDown.Click += new System.EventHandler(this.btnMoveDown_Click);
+            // 
+            // btnMoveUp
+            // 
+            this.btnMoveUp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnMoveUp.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(147)))), ((int)(((byte)(181)))), ((int)(((byte)(179)))));
+            this.btnMoveUp.Enabled = false;
+            this.btnMoveUp.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMoveUp.Location = new System.Drawing.Point(528, 12);
+            this.btnMoveUp.Name = "btnMoveUp";
+            this.btnMoveUp.Size = new System.Drawing.Size(88, 34);
+            this.btnMoveUp.TabIndex = 9;
+            this.btnMoveUp.Text = "Move Up";
+            this.btnMoveUp.UseVisualStyleBackColor = false;
+            this.btnMoveUp.Click += new System.EventHandler(this.btnMoveUp_Click);
+            // 
+            // btnMergeAll
+            // 
+            this.btnMergeAll.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnMergeAll.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(147)))), ((int)(((byte)(181)))), ((int)(((byte)(179)))));
+            this.btnMergeAll.Enabled = false;
+            this.btnMergeAll.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMergeAll.Location = new System.Drawing.Point(14, 385);
+            this.btnMergeAll.Name = "btnMergeAll";
+            this.btnMergeAll.Size = new System.Drawing.Size(156, 51);
+            this.btnMergeAll.TabIndex = 8;
+            this.btnMergeAll.Text = "Merge Sequential";
+            this.btnMergeAll.UseVisualStyleBackColor = false;
+            this.btnMergeAll.Click += new System.EventHandler(this.btnMerge_Click);
+            // 
+            // listFiles
+            // 
+            this.listFiles.AllowDrop = true;
+            this.listFiles.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listFiles.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(218)))), ((int)(((byte)(211)))));
+            this.listFiles.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listFiles.FormattingEnabled = true;
+            this.listFiles.ItemHeight = 16;
+            this.listFiles.Location = new System.Drawing.Point(12, 12);
+            this.listFiles.Name = "listFiles";
+            this.listFiles.Size = new System.Drawing.Size(510, 356);
+            this.listFiles.TabIndex = 7;
+            this.listFiles.DragDrop += new System.Windows.Forms.DragEventHandler(this.listFiles_DragDrop);
+            this.listFiles.DragEnter += new System.Windows.Forms.DragEventHandler(this.listFiles_DragEnter);
+            // 
+            // btnMergeADF
+            // 
+            this.btnMergeADF.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnMergeADF.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(147)))), ((int)(((byte)(181)))), ((int)(((byte)(179)))));
+            this.btnMergeADF.Enabled = false;
+            this.btnMergeADF.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMergeADF.Location = new System.Drawing.Point(188, 385);
+            this.btnMergeADF.Name = "btnMergeADF";
+            this.btnMergeADF.Size = new System.Drawing.Size(156, 51);
+            this.btnMergeADF.TabIndex = 13;
+            this.btnMergeADF.Text = "Merge ADF Odd/Even Mix";
+            this.btnMergeADF.UseVisualStyleBackColor = false;
+            this.btnMergeADF.Click += new System.EventHandler(this.btnMergeADF_Click);
+            // 
+            // btnUpdateThesis
+            // 
+            this.btnUpdateThesis.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnUpdateThesis.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(147)))), ((int)(((byte)(181)))), ((int)(((byte)(179)))));
+            this.btnUpdateThesis.Enabled = false;
+            this.btnUpdateThesis.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdateThesis.Location = new System.Drawing.Point(361, 385);
+            this.btnUpdateThesis.Name = "btnUpdateThesis";
+            this.btnUpdateThesis.Size = new System.Drawing.Size(156, 51);
+            this.btnUpdateThesis.TabIndex = 14;
+            this.btnUpdateThesis.Text = "Thesis Update";
+            this.btnUpdateThesis.UseVisualStyleBackColor = false;
+            this.btnUpdateThesis.Click += new System.EventHandler(this.btnUpdateThesis_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(218)))), ((int)(((byte)(211)))));
+            this.label1.Location = new System.Drawing.Point(253, 350);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(266, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "*for Thesis Update 1-Thesis 2-Signatures 3-Decleration";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(218)))), ((int)(((byte)(211)))));
+            this.label2.Location = new System.Drawing.Point(253, 328);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(149, 13);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "*for ADF Merge 1-Odd 2-Even";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(99)))), ((int)(((byte)(112)))), ((int)(((byte)(126)))));
+            this.ClientSize = new System.Drawing.Size(623, 454);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnUpdateThesis);
+            this.Controls.Add(this.btnMergeADF);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnMoveDown);
+            this.Controls.Add(this.btnMoveUp);
+            this.Controls.Add(this.btnMergeAll);
+            this.Controls.Add(this.listFiles);
+            this.Name = "Form1";
+            this.Text = "PDF Merger";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnMoveDown;
+        private System.Windows.Forms.Button btnMoveUp;
+        private System.Windows.Forms.Button btnMergeAll;
+        private System.Windows.Forms.ListBox listFiles;
+        private System.Windows.Forms.Button btnMergeADF;
+        private System.Windows.Forms.Button btnUpdateThesis;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ PdfMerger/MainFrm.cs
@@ -0,0 +1,414 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+
+namespace PDF571
+{
+    public partial class MainFrm : Form
+    {
+        public MainFrm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMoveUp_Click(object sender, EventArgs e)
+        {
+            if (listFiles.SelectedIndex >= 1)
+            {
+                int index = listFiles.SelectedIndex;
+                string s = (string)listFiles.SelectedItem;
+                listFiles.Items.RemoveAt(index);
+                listFiles.Items.Insert(index - 1, s);
+                listFiles.SelectedIndex = index - 1;
+            }
+        }
+
+        private void btnMoveDown_Click(object sender, EventArgs e)
+        {
+            if (listFiles.SelectedIndex != -1 && listFiles.SelectedIndex != listFiles.Items.Count - 1)
+            {
+                int index = listFiles.SelectedIndex;
+                string s = (string)listFiles.SelectedItem;
+                listFiles.Items.RemoveAt(index);
+                listFiles.Items.Insert(index + 1, s);
+                listFiles.SelectedIndex = index + 1;
+            }
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            if (listFiles.SelectedIndex != -1)
+            {
+                int index = listFiles.SelectedIndex;
+                listFiles.Items.RemoveAt(index);
+                if (index == listFiles.Items.Count)
+                {
+                    index--;
+                }
+                if (index >= 0)
+                {
+                    listFiles.SelectedIndex = index;
+                }
+            }
+            if (listFiles.Items.Count == 0)
+            {
+                btnMoveUp.Enabled = false;
+                btnMoveDown.Enabled = false;
+                btnRemove.Enabled = false;
+            }
+            if (listFiles.Items.Count <= 1)
+            {
+                btnMergeAll.Enabled = false;
+            }
+
+            if (listFiles.Items.Count !=2)
+            {
+                btnMergeADF.Enabled = false;
+            }
+
+            if (listFiles.Items.Count != 3)
+            {
+                btnUpdateThesis.Enabled = false;
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog d = new OpenFileDialog();
+            d.Multiselect = true;
+            d.Filter = "PDF Documents|*.pdf";
+            DialogResult dr = d.ShowDialog(this);
+
+            if (dr != DialogResult.Cancel && d.FileNames.Length > 0)
+            {
+                listFiles.Items.AddRange(d.FileNames);
+
+                btnMoveUp.Enabled = true;
+                btnMoveDown.Enabled = true;
+                btnRemove.Enabled = true;
+
+                if (listFiles.Items.Count > 1)
+                {
+                    btnMergeAll.Enabled = true;
+                }
+
+                if (listFiles.Items.Count == 2)
+                {
+                    btnMergeADF.Enabled = true;
+                }
+
+                if (listFiles.Items.Count == 3)
+                {
+                    btnUpdateThesis.Enabled = true;
+                }
+            }
+        }
+
+        private void btnMerge_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog s = new SaveFileDialog();
+            s.Filter = "PDF Documents|*.pdf";
+            s.AddExtension = true;
+            s.DefaultExt = "pdf";
+            DialogResult dr = s.ShowDialog(this);
+
+            if (dr != DialogResult.Cancel)
+            {
+                // Merge the PDFs
+                string[] files = new string[listFiles.Items.Count];
+                listFiles.Items.CopyTo(files, 0);
+
+                try
+                {
+                    // Merge the document...
+                    MergeSequentialScanning(s.FileName, files);
+                    //MergeDocuments(s.FileName, files);
+                    // ... and start a viewer
+                    Process.Start(s.FileName);
+                }
+                catch (UnauthorizedAccessException uaexc)
+                {
+                    MessageBox.Show("Error writing output document, perhaps you do not have write permission?" + Environment.NewLine +
+                                    "Error message: " + uaexc.Message,
+                        "Failed to write document",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error
+                    );
+                }
+                catch (System.IO.IOException ioexc)
+                {
+                    MessageBox.Show("Error writing output document, are you still viewing it in your PDF reader?." + Environment.NewLine +
+                                    "Error message: " + ioexc.Message,
+                        "Failed to write document",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error
+                    );
+                }
+            }
+        }
+
+        private void listFiles_DragEnter(object sender, DragEventArgs e)
+        {
+            // Only accept dropped files, not text etc
+            if (e.Data.GetDataPresent(DataFormats.FileDrop, false) == true)
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+        }
+
+        private void listFiles_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            bool allPDFs = true;
+            // Make sure only PDFs are dropped
+            foreach (string s in files)
+            {
+                if (!s.ToLower().EndsWith(".pdf"))
+                {
+                    allPDFs = false;
+                }
+            }
+
+            if (allPDFs && files.Length > 0)
+            {
+                listFiles.Items.AddRange(files);
+
+                btnMoveUp.Enabled = true;
+                btnMoveDown.Enabled = true;
+                btnRemove.Enabled = true;
+
+                if (listFiles.Items.Count > 1)
+                {
+                    btnMergeAll.Enabled = true;
+                }
+
+                if (listFiles.Items.Count == 2)
+                {
+                    btnMergeADF.Enabled = true;
+                }
+
+                if (listFiles.Items.Count == 3)
+                {
+                    btnUpdateThesis.Enabled = true;
+                }
+            }
+            else
+            {
+                MessageBox.Show("You can only add PDFs to the list! Please ensure all selected files are of type .PDF", "File error!", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+        }
+
+        /// <summary>
+        /// Imports all pages from a list of documents.
+        /// </summary>
+        public void ThesisUpdate(string outputFile, string[] files)
+        {
+            if (files.Length == 0) { return; }
+
+            PdfDocument outputMergedDocument = new PdfDocument();
+
+            PdfDocument inputThesis = PdfReader.Open(files[0], PdfDocumentOpenMode.Import);
+            PdfDocument inputSignatures = PdfReader.Open(files[1], PdfDocumentOpenMode.Import);
+            PdfDocument inputDeclaration = PdfReader.Open(files[2], PdfDocumentOpenMode.Import);
+
+
+            outputMergedDocument.Version = inputThesis.Version;
+            outputMergedDocument.Info.Title = "merged document";
+            outputMergedDocument.Info.Creator = "application";
+
+            int pageCount = inputThesis.PageCount;
+
+            for (int idx = 0; idx < pageCount; idx++)
+            {
+
+                if (idx == 2)
+                {
+                    outputMergedDocument.AddPage(inputSignatures.Pages[0]);
+                }
+                else if (idx == 3)
+                {
+                    outputMergedDocument.AddPage(inputDeclaration.Pages[0]);
+                }
+                else
+                {
+                    outputMergedDocument.AddPage(inputThesis.Pages[idx]);
+                }
+
+                
+            }
+
+            outputMergedDocument.Save(outputFile);
+
+            outputMergedDocument.Close();
+            inputThesis.Close();
+            inputSignatures.Close();
+            inputDeclaration.Close();
+
+        }
+
+        /// <summary>
+        /// Imports all pages from a list of documents.
+        /// </summary>
+        public void MergeAdfScanning(string outputFile, string[] files)
+        {
+            if (files.Length == 0) { return; }
+
+            PdfDocument outputMergedDocument = new PdfDocument();
+
+            PdfDocument inputDocumentOdd = PdfReader.Open(files[0], PdfDocumentOpenMode.Import);
+            PdfDocument inputDocumentEven = PdfReader.Open(files[1], PdfDocumentOpenMode.Import);
+
+            if (inputDocumentOdd.PageCount != inputDocumentEven.PageCount)
+            {
+                MessageBox.Show("Page Size Must Be Equal");
+                return;
+            }
+
+            outputMergedDocument.Version = inputDocumentOdd.Version;
+            outputMergedDocument.Info.Title = "merged document";
+            outputMergedDocument.Info.Creator = "application";
+
+            int pageCount = inputDocumentOdd.PageCount;
+
+            for (int idx = 0; idx < pageCount; idx++)
+            {
+                outputMergedDocument.AddPage(inputDocumentOdd.Pages[idx]);
+                outputMergedDocument.AddPage(inputDocumentEven.Pages[pageCount-idx-1]);
+            }
+
+            outputMergedDocument.Save(outputFile);
+
+            outputMergedDocument.Close();
+            inputDocumentOdd.Close();
+            inputDocumentEven.Close();
+
+        }
+
+        /// <summary>
+        /// Imports all pages from a list of documents.
+        /// </summary>
+        public void MergeSequentialScanning(string outputFile, string[] files)
+        {
+            if (files.Length == 0) { return; }
+
+            PdfDocument outputMergedDocument = new PdfDocument();
+
+
+            // Iterate over input files
+            foreach (string file in files)
+            {
+                // Open the document to import pages from it
+                PdfDocument inputDocument = PdfReader.Open(file, PdfDocumentOpenMode.Import);
+
+                // Iterate pages
+                int count = inputDocument.PageCount;
+                for (int i = 0; i < count; i++)
+                {
+                    outputMergedDocument.AddPage(inputDocument.Pages[i]);
+                }
+
+                inputDocument.Close();
+            }
+
+            outputMergedDocument.Save(outputFile);
+
+            outputMergedDocument.Close();
+
+        }
+
+        private void btnMergeADF_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog s = new SaveFileDialog();
+            s.Filter = "PDF Documents|*.pdf";
+            s.AddExtension = true;
+            s.DefaultExt = "pdf";
+            DialogResult dr = s.ShowDialog(this);
+
+            if (dr != DialogResult.Cancel)
+            {
+                // Merge the PDFs
+                string[] files = new string[listFiles.Items.Count];
+                listFiles.Items.CopyTo(files, 0);
+
+                try
+                {
+                    // Merge the document...
+                    MergeAdfScanning(s.FileName, files);
+                    // ... and start a viewer
+                    Process.Start(s.FileName);
+                }
+                catch (UnauthorizedAccessException uaexc)
+                {
+                    MessageBox.Show("Error writing output document, perhaps you do not have write permission?" + Environment.NewLine +
+                                    "Error message: " + uaexc.Message,
+                        "Failed to write document",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error
+                    );
+                }
+                catch (System.IO.IOException ioexc)
+                {
+                    MessageBox.Show("Error writing output document, are you still viewing it in your PDF reader?." + Environment.NewLine +
+                                    "Error message: " + ioexc.Message,
+                        "Failed to write document",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error
+                    );
+                }
+            }
+        }
+
+        private void btnUpdateThesis_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog s = new SaveFileDialog();
+            s.Filter = "PDF Documents|*.pdf";
+            s.AddExtension = true;
+            s.DefaultExt = "pdf";
+            DialogResult dr = s.ShowDialog(this);
+
+            if (dr != DialogResult.Cancel)
+            {
+                // Merge the PDFs
+                string[] files = new string[listFiles.Items.Count];
+                listFiles.Items.CopyTo(files, 0);
+
+                try
+                {
+                    // Merge the document...
+                    ThesisUpdate(s.FileName, files);
+                    // ... and start a viewer
+                    Process.Start(s.FileName);
+                }
+                catch (UnauthorizedAccessException uaexc)
+                {
+                    MessageBox.Show("Error writing output document, perhaps you do not have write permission?" + Environment.NewLine +
+                                    "Error message: " + uaexc.Message,
+                        "Failed to write document",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error
+                    );
+                }
+                catch (System.IO.IOException ioexc)
+                {
+                    MessageBox.Show("Error writing output document, are you still viewing it in your PDF reader?." + Environment.NewLine +
+                                    "Error message: " + ioexc.Message,
+                        "Failed to write document",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error
+                    );
+                }
+            }
+        }
+    }
+}
